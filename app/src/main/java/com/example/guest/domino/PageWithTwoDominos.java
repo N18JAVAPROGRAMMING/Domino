c52b93b561768f5a696cde7309ba78376b43e4e2
Ilya <ilya-kolomin2001@yandex.ru>
2019-01-13 00:19:15 +0300

    Добавил ViewPage в GameActivity, добавил GameActivity

---
+++ app/src/main/java/com/example/guest/domino/PageWithTwoDominos.java
@@ -0,0 +1,30 @@
+package com.example.guest.domino;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class PageWithTwoDominos extends Fragment {
+
+
+    public PageWithTwoDominos() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View view = inflater.inflate(R.layout.fragment_page_with_two_dominos, container, false);
+        return view;
+    }
+
+}
