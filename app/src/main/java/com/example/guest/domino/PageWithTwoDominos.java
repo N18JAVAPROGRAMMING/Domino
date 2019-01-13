a7d318b1a4fd6a70c7a088e0076666124d31f296
Ilya <ilya-kolomin2001@yandex.ru>
2019-01-13 18:48:36 +0300

    some changes in TwoDominosFragment

--- app/src/main/java/com/example/guest/domino/PageWithTwoDominos.java
+++ app/src/main/java/com/example/guest/domino/PageWithTwoDominos.java
@@ -17,10 +17,36 @@ public class PageWithTwoDominos extends Fragment {
     private Button button1;
     private Button button2;
 
+    private int left;
+    private int right;
+
+    public void setListener(OnFragmentClickListener listener) {
+        this.listener = listener;
+    }
+
+    OnFragmentClickListener listener;
+
+    public interface OnFragmentClickListener{
+        void onClick(int number);
+    }
+
     public PageWithTwoDominos() {
         // Required empty public constructor
     }
 
+    public void setNumbers(int left, int right){
+        this.left = left;
+        this.right = right;
+    }
+
+    public static PageWithTwoDominos newInstance() {
+        
+        Bundle args = new Bundle();
+        
+        PageWithTwoDominos fragment = new PageWithTwoDominos();
+        fragment.setArguments(args);
+        return fragment;
+    }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -29,7 +55,24 @@ public class PageWithTwoDominos extends Fragment {
         View view = inflater.inflate(R.layout.fragment_page_with_two_dominos, container, false);
         button1 = view.findViewById(R.id.button1);
         button2 = view.findViewById(R.id.button2);
+        setOnClickListeners();
         return view;
+    }
+
+    private void setOnClickListeners(){
+        button1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                listener.onClick(left);
+            }
+        });
+        button2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                listener.onClick(right);
+            }
+        });
     }
 
 }
